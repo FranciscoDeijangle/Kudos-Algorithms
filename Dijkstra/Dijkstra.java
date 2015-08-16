739d9aacd37e49a7ae84217167f00a58eaaaf0a1
ethen8181 <ethen8181@gamil.com>
2015-08-16 23:27:24 +0800

    shortestpath

---
+++ Dijkstra/Dijkstra.java
@@ -0,0 +1,146 @@
+/*  Dijkstra's shortest-path algorithm
+ *  File : Contains an adjacency list representation of an undirected weighted graph
+ *  Each row consists of nodes that're adjacent to that particular vertex ( the first entry ) and length of that edge  
+ *  e.g. the 6th row, has 6 in the first entry and 141,8200 as the next entry, 
+ *  this denotes there is an edge between vertex 6 and vertex 141 that has length 8200.
+ *  Task : compute shortest-path between 1( source vertex ) and the following ten vertices 
+ *  7,37,59,82,99,115,133,165,188,197 
+ */
+
+import java.io.*;
+import java.util.*;
+
+//stores the head and its respective tails, weights
+class Edges 
+{
+	int head;
+	Map< Integer, Integer > edges = new HashMap< Integer, Integer >(); 
+
+	Edges( int head ) 
+	{
+		this.head = head;
+	}
+
+	public void addEdge( int tail, int weight )
+	{
+		edges.put( tail, weight );
+	}
+	
+	public String toString()
+	{
+		return String.format("%d", head);
+	}
+}
+
+class DijkstraCalculation
+{
+	Edges[] adjacencylist;
+	int arraysize;
+	Map< Integer, Integer > shortestpath;
+	
+	DijkstraCalculation() throws IOException
+	{
+		// read in raw data
+		String filepath = "C:\\Users\\ASUS\\workspace\\Algorithm\\src\\Dijkstra.txt";
+		//String filepath = "C:\\Users\\ASUS\\workspace\\Algorithm\\src\\Dijkstratest.txt";
+		Scanner scanner = new Scanner( new File( filepath ) );
+		List<String> data = new ArrayList<String>();
+		
+		while( scanner.hasNextLine() )
+			data.add( scanner.nextLine() );
+		scanner.close();
+		
+		// initiate adjacency list array, add 1 to the size because there're no node 0
+		arraysize = data.size()+1;
+		adjacencylist = new Edges[arraysize]; 
+		shortestpath  = new HashMap< Integer, Integer >();
+		
+		// set up the adjacency list and the shortestpath
+		for( String string : data )
+		{
+			String[] linesplit = string.split("\\s+");
+			int head = Integer.valueOf( linesplit[0] );
+			Edges edge = new Edges(head);
+			
+			for( int l = 1; l < linesplit.length; l++ )
+			{
+				StringTokenizer str = new StringTokenizer( linesplit[l], "," );
+				int tail   = Integer.valueOf( str.nextToken() );
+				int weight = Integer.valueOf( str.nextToken() );
+				edge.addEdge( tail, weight );
+			}
+			adjacencylist[head] = edge;
+			
+			shortestpath.put( head, null );
+		}
+	}
+	
+	public int[] calculationProcess( int source )
+	{
+		// initiate distance of the array, the source vertex is initiated as 0 
+		int[] shortestdistance = new int[arraysize];
+		Arrays.fill( shortestdistance, Integer.MAX_VALUE );
+		shortestdistance[source] = 0;
+		
+		// initiate the queue with the source node 
+		Queue<Integer> node = new PriorityQueue<Integer>();
+		node.add(source);
+		
+		// visit each existing edges of each node, if the cumulative distance to the tail of that node is smaller than 
+		// the current shortest path, update shortest path distance to the tail of that node 
+		// also update the shortest path to the head of that node
+		while( !node.isEmpty() )
+		{
+			int head = node.poll();
+			for( Integer tail : adjacencylist[head].edges.keySet() )
+			{
+				int edgelength = adjacencylist[head].edges.get(tail);
+				if( shortestdistance[tail] > shortestdistance[head] + edgelength )
+				{
+					shortestdistance[tail] = shortestdistance[head] + edgelength;
+					node.add(tail);
+					shortestpath.put( tail, head );
+				}
+			}
+		}
+		return shortestdistance;
+	}
+	
+    public List<Integer> getShortestPath( int source, int destination )
+    {
+    	List<Integer> path = new ArrayList<Integer>();
+    	// back trace the shortest path from the destination to the source
+    	while( shortestpath.get(destination) != null )
+    	{
+    		path.add(destination);
+    		int node = shortestpath.get(destination);
+    		destination = node;
+     	}
+    	path.add(source);
+    	
+        Collections.reverse(path);
+        return path;
+    } 
+}
+
+public class Dijkstra
+{
+	public static void main(String[] args) throws IOException
+	{
+		DijkstraCalculation calculation = new DijkstraCalculation();
+		// pass the source node
+		int source = 1;
+		int[] shortestdistance = calculation.calculationProcess(source); 
+		
+		// print the shortest path distances to the specified destination
+		int[] destinations = { 7, 37, 59, 82, 99, 115, 133, 165, 188, 197 };
+		for( int node : destinations )
+			System.out.print( shortestdistance[node] + "," );
+		System.out.println();
+		
+		// print the traversed nodes of the shortest path from source to destination node
+		System.out.println( calculation.getShortestPath( source, 7 ) );
+	}
+}
+
+
--- README.md
+++ README.md
@@ -1,26 +1,32 @@
 # Kudos-Algorithms
 
-**ArrayInversion** last updated 2015.7.6
+**ArrayInversion** 2015.7.6
 
 - `ArrayInversion.java` Merge Sort for counting number of inversions in an given unsorted array.
 - `ArrayInversion.txt`  The raw array elements used in code.
 
-**KNN** last updated 2015.7.1
+**Dijkstra** 2015.8.16
+
+- `Dijkstra.java` Dijkstra Algorithm for computing shortest path of a source node to a destination node of a directed graph.
+- `Dijkstra.txt`  Adjacency list of the graph, including the weight of the edges.
+
+
+**KNN** 2015.7.1
 
 - `KNN.java` K-nearest neighborhood in predicting class labels. 
-- Uses the data in the training file to "train" a model and predict the class label for in testing set in the testing file.
+- Uses the data in the training file to "train" a model and predict the class label for the testing set in the testing file.
 
-**MinCut:** last updated 2015.7.26
+**MinCut:** 2015.7.26
 
 - `GraphAdjacencyList.java` Simple demonstration on adjancency list with graph.
 - `MinCut.java` Using randomized contraction algorithm for the min cut problem.
 - `MinCut.txt`  The raw adjacency list data used in code.
 
-**QuickSort:** last updated 2015.7.24
+**QuickSort:** 2015.7.24
 
 - `QuickSort1.java` Implemenation of quicksort algorithm on a example unsorted array, while the pivot element is chosen by the median of three method. 
 
-**SCC:** last updated 2015.8.6
+**SCC:** 2015.8.6
 
 - `SCC.java` Kosaraju's algorithm. Calculates strongly connected component of a directed graph. 
-- `SCC.txt`  data that stores the edges of a directed graph.
+- `SCC.txt`  Data that stores the edges of a directed graph.
